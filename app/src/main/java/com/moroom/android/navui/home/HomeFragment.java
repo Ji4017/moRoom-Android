422ae82fa29e0557a9d4b42f7cf289eb1340b895
jihoon <bak4017@gmail.com>
2024-04-23 01:07:12 +0900

    refactor: HomeFragment 코드 함수 분리

--- app/src/main/java/com/moroom/android/navui/home/HomeFragment.java
+++ app/src/main/java/com/moroom/android/navui/home/HomeFragment.java
@@ -6,6 +6,7 @@ import static android.app.Activity.RESULT_OK;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -48,71 +49,55 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
+        setupViews();
+        setupListener();
 
-        ImageView mainImageView = binding.mainImage;
-        EditText searchView = binding.etAddress;
+        return view;
+    }
 
-        RecyclerView recyclerView = view.findViewById(R.id.homeRecyclerView);
-        recyclerView.setHasFixedSize(true);
+    private void setupViews() {
+        setupHomeImg();
+        setupRecyclerView();
+        setupMainReviews();
+    }
+
+    private void setupHomeImg() {
+        binding.imgHome.setClipToOutline(true);
+    }
+
+    private void setupRecyclerView() {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
-        recyclerView.setLayoutManager(layoutManager);
+        binding.homeRecyclerView.setHasFixedSize(true);
+        binding.homeRecyclerView.setLayoutManager(layoutManager);
+    }
 
-        FloatingActionButton fab = binding.fab;
-
-        LinearLayout linearUam = binding.linearUam;
-        LinearLayout linearYeJi = binding.linearYeJi;
-        LinearLayout linearGukJe = binding.linearGukJe;
-        LinearLayout linearJinWon = binding.linearJinWon;
-
-        linearUam.setOnClickListener(view1 -> {
-            Intent intent = new Intent(getActivity(), ResultActivity.class);
-            intent.putExtra("searchedAddress", "충북 청주시 청원구 안덕벌로19번길 116 (내덕동) 우암마을");
-            startActivity(intent);
-        });
-
-        linearYeJi.setOnClickListener(view1 -> {
-            Intent intent = new Intent(getActivity(), ResultActivity.class);
-            intent.putExtra("searchedAddress", "충북 청주시 청원구 안덕벌로19번길 116 (내덕동) 예지관");
-            startActivity(intent);
-        });
-
-        linearGukJe.setOnClickListener(view1 -> {
-            Intent intent = new Intent(getActivity(), ResultActivity.class);
-            intent.putExtra("searchedAddress", "충북 청주시 청원구 안덕벌로19번길 116 (내덕동) 국제학사");
-            startActivity(intent);
-        });
-
-        linearJinWon.setOnClickListener(view1 -> {
-            Intent intent = new Intent(getActivity(), ResultActivity.class);
-            intent.putExtra("searchedAddress", "충북 청주시 청원구 수암로66번길 48-2 (우암동, 한진 신세대 아파트)");
-            startActivity(intent);
-        });
-
-
-        mainImageView.setClipToOutline(true);
-
-        searchView.setFocusable(false);
-        searchView.setOnClickListener(view1 -> {
+    private void setupListener() {
+        binding.etSearch.setFocusable(false);
+        binding.etSearch.setOnClickListener(view1 -> {
             Intent intent = new Intent(getActivity(), SearchActivity.class);
             getSearchResult.launch(intent);
         });
 
-
-        fab.setOnClickListener(view12 -> {
+        binding.fab.setOnClickListener(view12 -> {
             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
             Intent intent;
-            if (user != null) {
-                intent = new Intent(getActivity(), WriteActivity.class);
-            } else {
-                intent = new Intent(getActivity(), MoveToLogin.class);
-            }
+
+            if (user != null) intent = new Intent(getActivity(), WriteActivity.class);
+            else intent = new Intent(getActivity(), MoveToLogin.class);
+
             startActivity(intent);
         });
 
+        binding.linearUam.setOnClickListener(view -> navigateToDormitory("충북 청주시 청원구 안덕벌로19번길 116 (내덕동) 우암마을"));
+        binding.linearYeJi.setOnClickListener(view -> navigateToDormitory("충북 청주시 청원구 안덕벌로19번길 116 (내덕동) 예지관"));
+        binding.linearGukJe.setOnClickListener(view -> navigateToDormitory("충북 청주시 청원구 안덕벌로19번길 116 (내덕동) 국제학사"));
+        binding.linearJinWon.setOnClickListener(view -> navigateToDormitory("충북 청주시 청원구 수암로66번길 48-2 (우암동, 한진 신세대 아파트)"));
+    }
+
+    private void setupMainReviews() {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = database.getReference("Address").child("reviewForMain");
         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -122,34 +107,27 @@ public class HomeFragment extends Fragment {
                 for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     Contents contents = dataSnapshot.getValue(Contents.class);
                     arrayList.add(contents);
-
-                    // Log.d("HomeFragment", "DB data : " + dataSnapshot.getValue());
-                    // Firebase DB에 있는 데이터 로그로 출력
                 }
                 adapter.notifyDataSetChanged();
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                // Log.e("HomeFragment", String.valueOf(error.toException()));
+                 Log.e("HomeFragment", String.valueOf(error.toException()));
             }
         });
 
-        Contents contents = new Contents();
-        contents.setBlur(false);
-
-        Context context = getActivity();
-        adapter = new ContentsAdapter(arrayList, context);
-        recyclerView.setAdapter(adapter);
-
-
-        return view;
+        setupAdapter();
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private void setupAdapter() {
+        adapter = new ContentsAdapter(arrayList, getActivity());
+        binding.homeRecyclerView.setAdapter(adapter);
+    }
+
+    private void navigateToDormitory(String address) {
+        Intent intent = new Intent(getActivity(), ResultActivity.class);
+        intent.putExtra("searchedAddress", address);
+        startActivity(intent);
     }
 
     private final ActivityResultLauncher<Intent> getSearchResult = registerForActivityResult(
@@ -168,4 +146,10 @@ public class HomeFragment extends Fragment {
                 }
             }
     );
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
