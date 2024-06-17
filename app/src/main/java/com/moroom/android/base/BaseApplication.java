2ae41bd355af43c71eb96afd197fa1472bf27238
jihoon <bak4017@gmail.com>
2024-06-17 16:17:04 +0900

    refactor: 카카오 SDK 인증 방법 변경

--- app/src/main/java/com/moroom/android/base/BaseApplication.java
+++ app/src/main/java/com/moroom/android/base/BaseApplication.java
@@ -1,7 +1,11 @@
 package com.moroom.android.base;
 
 import android.app.Application;
+
 import androidx.appcompat.app.AppCompatDelegate;
+
+import com.kakao.sdk.common.KakaoSdk;
+import com.moroom.android.BuildConfig;
 
 public class BaseApplication extends Application {
     @Override
@@ -9,5 +13,8 @@ public class BaseApplication extends Application {
         super.onCreate();
         // 다크 모드 비활성화
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        // Kakao SDK 초기화
+        KakaoSdk.init(this, BuildConfig.KAKAO_SDK_APPKEY);
     }
 }
