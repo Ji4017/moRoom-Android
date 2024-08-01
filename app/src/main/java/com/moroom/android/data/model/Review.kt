5cc83b95940b387234865a976869d33aafb8f95f
jihoon <bak4017@gmail.com>
2024-08-01 23:58:41 +0900

    refactor: 리뷰 모델 클래스를 코틀린의 데이터 클래스로 변경

---
+++ app/src/main/java/com/moroom/android/data/model/Review.kt
@@ -0,0 +1,8 @@
+package com.moroom.android.data.model
+
+data class Review(
+    val title: String = "",
+    val checkedItems: String = "",
+    val pros: String = "",
+    val cons: String = ""
+)
