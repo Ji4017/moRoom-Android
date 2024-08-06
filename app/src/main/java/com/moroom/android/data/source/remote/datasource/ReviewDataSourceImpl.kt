c0808fa69087a4319addf944a1abfedde76de68e
jihoon <bak4017@gmail.com>
2024-08-06 14:53:14 +0900

    refactor: 리뷰 저장 시 에러 로그 Crashlytics에 기록하도록 수정

--- app/src/main/java/com/moroom/android/data/source/remote/datasource/ReviewDataSourceImpl.kt
+++ app/src/main/java/com/moroom/android/data/source/remote/datasource/ReviewDataSourceImpl.kt
@@ -1,5 +1,8 @@
 package com.moroom.android.data.source.remote.datasource
 
+import androidx.compose.runtime.key
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.google.firebase.database.FirebaseDatabase
 import com.moroom.android.data.source.remote.model.Review
 import kotlinx.coroutines.Dispatchers
@@ -8,12 +11,24 @@ import javax.inject.Inject
 
 class ReviewDataSourceImpl @Inject constructor(
     private val database: FirebaseDatabase,
+    private val firebaseAuth: FirebaseAuth,
+    private val crashlytics: FirebaseCrashlytics
 ) : ReviewDataSource {
     override suspend fun saveReviewToFirebase(review: Review): Unit =
         withContext(Dispatchers.IO) {
-            database.getReference("Address")
-                .child(review.address)
-                .push()
-                .setValue(review)
+            try {
+                database.getReference("Address")
+                    .child(review.address)
+                    .push()
+                    .setValue(review)
+            } catch (error: Exception) {
+                crashlytics.setUserId(firebaseAuth.uid!!)
+                crashlytics.setCustomKey("title", review.title)
+                crashlytics.setCustomKey("checkedItems", review.checkedItems)
+                crashlytics.setCustomKey("pros", review.pros)
+                crashlytics.setCustomKey("cons", review.cons)
+                crashlytics.setCustomKey("createdAt", review.createdAt)
+                crashlytics.recordException(error)
+            }
         }
 }
