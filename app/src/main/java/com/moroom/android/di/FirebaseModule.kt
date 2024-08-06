adaf4daa17bb15e5d865507f379e28d7f67f19b8
jihoon <bak4017@gmail.com>
2024-08-06 13:59:11 +0900

    feat: Crashlytics 모듈 추가

--- app/src/main/java/com/moroom/android/di/FirebaseModule.kt
+++ app/src/main/java/com/moroom/android/di/FirebaseModule.kt
@@ -1,7 +1,10 @@
 package com.moroom.android.di
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.crashlytics.FirebaseCrashlytics
+import com.google.firebase.crashlytics.ktx.crashlytics
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -20,5 +23,11 @@ object FirebaseModule {
     @Provides
     fun provideFirebaseAuth(): FirebaseAuth {
         return FirebaseAuth.getInstance()
+    }
+
+    @Provides
+    @Singleton
+    fun provideFirebaseCrashlytics(): FirebaseCrashlytics {
+        return Firebase.crashlytics
     }
 }
