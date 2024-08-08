44d72b1481cc9384de002b0d4714de3edc587f36
jihoon <bak4017@gmail.com>
2024-08-09 00:36:41 +0900

    feat: 회원가입 Repository 구현

--- app/src/main/java/com/moroom/android/data/repository/AccountRepositoryImpl.kt
+++ app/src/main/java/com/moroom/android/data/repository/AccountRepositoryImpl.kt
@@ -1,10 +1,28 @@
 package com.moroom.android.data.repository
 
+import com.google.firebase.auth.FirebaseAuth
 import com.moroom.android.data.source.remote.datasource.AccountDataSource
+import com.moroom.android.data.source.remote.model.UserAccount
 import com.moroom.android.domain.repository.AccountRepository
 import javax.inject.Inject
 
-class AccountRepositoryImpl @Inject constructor(private val accountDataSource: AccountDataSource) :
+class AccountRepositoryImpl @Inject constructor(
+    private val accountDataSource: AccountDataSource,
+    private val firebaseAuth: FirebaseAuth
+) :
     AccountRepository {
     override suspend fun updateUserReviewStatus() = accountDataSource.updateUserReviewStatus()
+
+    override suspend fun createUserInAuthentication(email: String, password: String): Int =
+        accountDataSource.createUserInAuthentication(email, password)
+
+    override suspend fun createUserInDB(email: String, password: String): Int {
+        val userAccount = UserAccount()
+        userAccount.email = email
+        userAccount.password = password
+        userAccount.idToken = firebaseAuth.uid
+        userAccount.isReview = false
+        val resultCode = accountDataSource.createUserInDB(userAccount)
+        return resultCode
+    }
 }
--- app/src/main/java/com/moroom/android/domain/repository/AccountRepository.kt
+++ app/src/main/java/com/moroom/android/domain/repository/AccountRepository.kt
@@ -2,4 +2,6 @@ package com.moroom.android.domain.repository
 
 interface AccountRepository {
     suspend fun updateUserReviewStatus()
+    suspend fun createUserInAuthentication(email: String, password: String): Int
+    suspend fun createUserInDB(email: String, password: String): Int
 }
