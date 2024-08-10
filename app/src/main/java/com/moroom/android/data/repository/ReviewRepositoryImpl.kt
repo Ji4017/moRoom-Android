1282605201f154b548e546684be076d297c9aa3a
jihoon <bak4017@gmail.com>
2024-08-10 20:52:29 +0900

    feat: ReviewRepository에 리뷰가져오는 함수 및  리뷰담을 변수 추가

--- app/src/main/java/com/moroom/android/data/repository/ReviewRepositoryImpl.kt
+++ app/src/main/java/com/moroom/android/data/repository/ReviewRepositoryImpl.kt
@@ -4,14 +4,22 @@ import com.moroom.android.data.source.remote.mapper.DataMappers
 import com.moroom.android.data.source.remote.datasource.ReviewDataSource
 import com.moroom.android.domain.model.WrittenReview
 import com.moroom.android.domain.repository.ReviewRepository
+import com.moroom.android.presentation.result.ReviewState
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 class ReviewRepositoryImpl @Inject constructor(
     private val reviewDataSource: ReviewDataSource,
     private val dataMappers: DataMappers
 ) : ReviewRepository {
-    override suspend fun saveReviewToFireBase(writtenReview: WrittenReview) {
+    override val reviewsState: StateFlow<ReviewState> = reviewDataSource.reviewsState
+
+    override suspend fun fetchReview(address: String) {
+        reviewDataSource.fetchReview(address)
+    }
+
+    override suspend fun saveReview(writtenReview: WrittenReview) {
         val reviewModel = dataMappers.mapToDataModel(writtenReview)
-        reviewDataSource.saveReviewToFirebase(reviewModel)
+        reviewDataSource.saveReview(reviewModel)
     }
 }
--- app/src/main/java/com/moroom/android/domain/repository/ReviewRepository.kt
+++ app/src/main/java/com/moroom/android/domain/repository/ReviewRepository.kt
@@ -1,7 +1,11 @@
 package com.moroom.android.domain.repository
 
 import com.moroom.android.domain.model.WrittenReview
+import com.moroom.android.presentation.result.ReviewState
+import kotlinx.coroutines.flow.StateFlow
 
 interface ReviewRepository {
-    suspend fun saveReviewToFireBase(writtenReview: WrittenReview)
+    val reviewsState: StateFlow<ReviewState>
+    suspend fun fetchReview(address: String)
+    suspend fun saveReview(writtenReview: WrittenReview)
 }
