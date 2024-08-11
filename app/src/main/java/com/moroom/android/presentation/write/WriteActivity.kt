1fc188407834f822e43934dd0876eb3e00b5aff2
jihoon <bak4017@gmail.com>
2024-08-12 00:46:13 +0900

    refactor: 오탈자 세미콜론 제거

--- app/src/main/java/com/moroom/android/presentation/write/WriteActivity.kt
+++ app/src/main/java/com/moroom/android/presentation/write/WriteActivity.kt
@@ -1,9 +1,9 @@
-package com.moroom.android.presentation.write;
+package com.moroom.android.presentation.write
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import dagger.hilt.android.AndroidEntryPoint;
+import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class WriteActivity : ComponentActivity() {
