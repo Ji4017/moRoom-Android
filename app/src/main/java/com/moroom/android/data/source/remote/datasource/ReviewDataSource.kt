e5c86ed24234c847c5c6eee43806d074e90cbd4a
jihoon <bak4017@gmail.com>
2024-08-13 16:24:56 +0900

    refactor: ReviewState 자료형을 StateFlow에서 Flow로 변경

--- app/src/main/java/com/moroom/android/data/source/remote/datasource/ReviewDataSource.kt
+++ app/src/main/java/com/moroom/android/data/source/remote/datasource/ReviewDataSource.kt
@@ -3,10 +3,9 @@ package com.moroom.android.data.source.remote.datasource
 import com.moroom.android.data.source.remote.model.Review
 import com.moroom.android.presentation.result.ReviewState
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.StateFlow
 
 interface ReviewDataSource {
-    val reviewsState: StateFlow<ReviewState>
+    val reviewsState: Flow<ReviewState>
     suspend fun fetchReview(address: String)
     suspend fun saveReview(review: Review)
 }
--- app/src/main/java/com/moroom/android/domain/repository/ReviewRepository.kt
+++ app/src/main/java/com/moroom/android/domain/repository/ReviewRepository.kt
@@ -2,10 +2,10 @@ package com.moroom.android.domain.repository
 
 import com.moroom.android.domain.model.WrittenReview
 import com.moroom.android.presentation.result.ReviewState
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.Flow
 
 interface ReviewRepository {
-    val reviewsState: StateFlow<ReviewState>
+    val reviewsState: Flow<ReviewState>
     suspend fun fetchReview(address: String)
     suspend fun saveReview(writtenReview: WrittenReview)
 }
