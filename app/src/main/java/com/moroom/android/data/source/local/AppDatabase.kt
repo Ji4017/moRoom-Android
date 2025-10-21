c0980dae5763095991147da1c98c05dc4d377222
Ji4017 <qeq151@gmail.com>
2025-10-21 19:38:07 +0900

    chore: Gradle 버전 업에 따라 문법 수정 및 라이브러리 업데이트

--- app/src/main/java/com/moroom/android/data/source/local/AppDatabase.kt
+++ app/src/main/java/com/moroom/android/data/source/local/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [BestReview::class], version = 1)
+@Database(entities = [BestReview::class], version = 1, exportSchema = false)
 abstract class AppDatabase: RoomDatabase() {
     abstract fun BestReviewDao(): BestReviewDao
 
